3cbe89f0a3c275f5cf8d51e2b7aa0004ee1e9dd4
monst12 <Joshua.Doll@online.de>
2016-11-19 20:13:51 +0100

    Finished Homework

--- src/Exponential.java
+++ src/Exponential.java
@@ -14,49 +14,84 @@ public class Exponential{
         
         System.out.println("Bitte geben Sie eine ganze Zahl als Summationsgrenze N ein.");
         N = sc.nextInt(); //Summationsgrenze
+        System.out.println("\n");
         
-        //
+        limExp(N, x);
+        compare(x);
+        exp(x);
         
+        sc.close();
+    } 
+    
+    public static void limExp(int N, double x){
         double total = 1;
+        double rx = Math.abs(x);
         //Additive Schleife
         for(int j = 1; j < N; j++){
             double curr = 1;
             //Multiplikative Schleife: x...x/N!
             for(int y = j; y >  0; y--){
-                double yj = (double) x/y;
+                double yj = (double) rx/y;
                 curr*=yj;
             } 
             total+=curr; 
         }
         
-        System.out.println("S(N="+N+", x="+x+") = "+total);
+        if(x < 0){
+        	total = 1.0/total;
+        }
         
-        //
-        N = 0;
+        System.out.println("S(N="+N+", x="+x+") = "+total);
+        System.out.println("--======================================--\n");
+    }
+    
+    public static void compare(double x){
+        double rx = Math.abs(x);
+        int N = 1;
         double firsttotal = 1;
-        double secondtotal = 1;
-        for(int j = 1;j < 10; j++){
-            double curr = 1;
-            
-            for(int y = j; y > 0; y--){
-                double yj = (double) x/y;
-                curr*=yj;
+        double sectotal = 1;
+
+    	for(;;){
+            //Additive Schleife 1
+            for(int j = 1; j < N; j++){
+                double curr = 1;
+                //Multiplikative Schleife: x...x/N!
+                for(int y = j; y >  0; y--){
+                    double yj = (double) rx/y;
+                    curr*=yj;
+                } 
+                firsttotal+=curr; 
             }
             
-            firsttotal+=curr;  
-            double yj = (double) x/(j+1);
-            curr*=yj;
-            secondtotal = firsttotal+=curr;
-            
-            curr = 1;
+            //Inkrement
             N++;
             
-            System.out.println("1. total: "+firsttotal+" 2. total: "+secondtotal);
-            double abs = Math.abs(firsttotal-secondtotal);
-            System.out.println("abs "+abs);
+            //Additive Schleife 1
+            for(int j = 1; j < N; j++){
+                double curr = 1;
+                //Multiplikative Schleife: x...x/N!
+                for(int y = j; y >  0; y--){
+                    double yj = (double) rx/y;
+                    curr*=yj;
+                } 
+                sectotal+=curr; 
+            }
+            
+            double abs = Math.abs(firsttotal-sectotal);
+
             if(abs <= 10e-13)
-                break;
-        }
-        sc.close();
-    } 
+            	break;
+            
+            firsttotal = 1;
+            sectotal = 1;
+    	}
+    	
+    	System.out.println("Benötigte Summationen N: "+N);
+    	System.out.println("--======================================--\n");
+    }
+    
+    public static void exp(double x){
+    	System.out.println("Math.exp(x) "+Math.exp(x));
+        System.out.println("--======================================--");
+    }
 }
